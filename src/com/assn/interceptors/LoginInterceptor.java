0ee2c7065b53b54d75ab662efaea903a37c61311
yangkang0121 <yangkang0121@163.com>
2017-04-10 01:26:32 +0800

    验证码生成器以及登陆状态检测

---
+++ src/com/assn/interceptors/LoginInterceptor.java
@@ -0,0 +1,36 @@
+package com.assn.interceptors;
+
+import org.springframework.web.servlet.HandlerInterceptor;
+import org.springframework.web.servlet.ModelAndView;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.util.Objects;
+
+/**
+ * Created by Heal on 2017/4/9.
+ */
+public class LoginInterceptor implements HandlerInterceptor {
+    @Override
+    public boolean preHandle(HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse, Object o) throws Exception {
+        if(Objects.isNull(httpServletRequest.getSession().getAttribute("user"))){
+            httpServletRequest.getRequestDispatcher("/").forward(httpServletRequest,httpServletResponse);
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    public void postHandle(HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse, Object o, ModelAndView modelAndView) throws Exception {
+
+    }
+
+    @Override
+    public void afterCompletion(HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse, Object o, Exception e) throws Exception {
+        if (Objects.isNull(e)){
+//            调用方法未出现异常的操作
+        }else{
+//            有异常时对应的操作
+        }
+    }
+}
